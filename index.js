cbaebb8b30ee934408b6ee0ca48f9854ac901ef1
Ken Klooster <kklooster3910@gmail.com>
2019-10-31 15:46:50 -0400

    server and graphql set up, except for graphql schema

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const app = express(); // creating a new express server
+const db = require('./config/keys').mongoURI;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const expressGraphQL = require('express-graphql')
 
-app.get('/', (req, res) => res.send("Hello you fucking world"));
+
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Successfully connected to mongoDB fam'))
+  .catch(err => console.log(err));
+
+// app.get('/', (req, res) => res.send("Hello to everybody out there, testing nodemon"));
+app.use(
+  "/graphql",
+  expressGraphQL({
+    graphiql: true
+  })
+)
+app.use(bodyParser.json());
 app.listen(5000, () => console.log('server is running on port 5000 fam'))
 
--- models/User.js
+++ models/User.js
@@ -1,3 +1,27 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const userSchema = new Schema({
+  name: {
+    type: String,
+    requuired: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  posts: [{
+    type: Schema.Types.ObjectId,
+    ref: 'post'
+  }]
+})
+
+module.exports = mongoose.model('user', userSchema);
